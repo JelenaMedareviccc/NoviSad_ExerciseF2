391a28fd4516e864138f6f089f5e0ffd98cbbfc8
somename <someemail>
2017-05-12 14:34:28 +0200

    besouro automatic message

--- src/PlanetExplorerException.java
+++ src/PlanetExplorerException.java
@@ -1,4 +1,14 @@
 
 public class PlanetExplorerException extends Exception {
 
+	public PlanetExplorerException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PlanetExplorerException(Throwable message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
 }
