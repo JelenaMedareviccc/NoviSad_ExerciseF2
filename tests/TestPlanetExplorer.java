b877a09771cbe0276396c95dcf1b833ec765759e
somename <someemail>
2017-05-12 14:54:36 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_commandL() {
+	public void test_commandF() {
 		PlanetExplorer pe=new PlanetExplorer(0,0,"");
 		
-		assertEquals("(0,1,N)",pe.executeCommand("l"));
+		assertEquals("(0,1,N)",pe.executeCommand("f"));
+		
+	}
+	
+	@Test
+	public void test_commandB() {
+		PlanetExplorer pe=new PlanetExplorer(0,1,"");
+		
+		assertEquals("(0,0,N)",pe.executeCommand("b"));
 		
 	}
 }
